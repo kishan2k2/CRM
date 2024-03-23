cf2e40479b16470bec0ae963444ef663fade122c
Kishan <payadikishan@gmail.com>
2024-03-24 00:26:47 +0530

    Basic templating is done, next thing to do is learning about the logout endpoint and doing the other part of authentication and modelling work.

--- CRM/app/views.py
+++ CRM/app/views.py
@@ -9,4 +9,18 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        confirm = request.POST.get('password2')
+        print(password)
+        print(confirm)
+        if password!=confirm:
+            return render(request, 'register.html', {
+                "error":"Password didn't match try again"
+            })
+        
     return render(request, 'register.html')
+
+def logout(request):
+    return render(request, 'logout.html')
