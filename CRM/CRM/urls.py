727f1d549b0638c53bc53e9f709f78d032bc6d16
Kishan <payadikishan@gmail.com>
2024-03-27 00:44:17 +0530

    Login logout and sycnronisation is done.

--- CRM/CRM/urls.py
+++ CRM/CRM/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app', include('app.urls'))
+    path('app', include('app.urls')),
+    path('accounts/login/', login, name='login'),    # Login page
+
 ]
--- CRM/app/views.py
+++ CRM/app/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import * # If it doesn't work out then I will import User directly from django.contrib.authenticate itself.\
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages # What is this message for?
+from django.contrib.auth import authenticate, login as auth_login, logout as Logout
 
 # Create your views here.
-
+username = ""
 def home(request):
+    if username != "":
+        return render(request, 'home.html', {
+            'username': f'{username}'
+        })
     return render(request, 'home.html')
 
 def login(request):
+    if request.method == 'POST':
+        global username
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid user')
+            redirect('/app/register')
+        else:
+            user = authenticate(username = username, password = password)
+            if user is None:
+                messages.error(request, 'Invalid password')
+                return redirect('/app/login')
+            else:
+                auth_login(request, user)
+                # redirect('/app/')
+                return render(request, 'home.html', {
+                    'username': f'{username}'
+                })
+            
+
     return render(request, 'login.html')
 
 def register(request):
@@ -19,8 +48,27 @@ def register(request):
             return render(request, 'register.html', {
                 "error":"Password didn't match try again"
             })
+        user = User.objects.filter(username = username)
+        print("Username is ", username)
+        if user.exists():
+            messages.info(request, "User name already taken") # What does this do I dont understand lets watch.
+            print("Username already exist.")
+            return redirect('/app/register')
+        else:
+            user = User.objects.create_user(
+                username = username
+            )
+            user.set_password(password)
+            user.save()
+            messages.info(request, 'Account created sucessfully')
+            print("new account created")
+            return redirect('/app/login')
+            
         
     return render(request, 'register.html')
-
+@login_required
 def logout(request):
-    return render(request, 'logout.html')
+    global username
+    username = ""
+    Logout(request)
+    return redirect('/app/')
