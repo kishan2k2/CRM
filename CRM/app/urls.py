f35db6eb2e8bc124f5dbbc239720147988b7e839
Kishan <payadikishan@gmail.com>
2024-03-27 07:26:55 +0530

    forget password done.

--- CRM/app/urls.py
+++ CRM/app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('/login/', views.login, name = 'login'),
     path('/register/', views.register, name = 'register'),
     path('/logout/', views.logout, name = 'logout'),
+    path('reset/', views.reset, name = 'reset'),
+    path('setpass/<str:username>', views.setpass, name = 'setpass'),
+    path('confirm/<int:key>/<str:username>', views.confirm, name = 'confirm'),
 ]
--- CRM/app/views.py
+++ CRM/app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages # What is this message for?
 from django.contrib.auth import authenticate, login as auth_login, logout as Logout
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 username = ""
@@ -21,7 +22,7 @@ def login(request):
         password = request.POST.get('password')
         if not User.objects.filter(username = username).exists():
             messages.error(request, 'Invalid user')
-            redirect('/app/register')
+            return redirect('/app/register')
         else:
             user = authenticate(username = username, password = password)
             if user is None:
@@ -40,6 +41,7 @@ def login(request):
 def register(request):
     if request.method == 'POST':
         username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password1')
         confirm = request.POST.get('password2')
         print(password)
@@ -56,7 +58,8 @@ def register(request):
             return redirect('/app/register')
         else:
             user = User.objects.create_user(
-                username = username
+                username = username,
+                email=email,
             )
             user.set_password(password)
             user.save()
@@ -66,6 +69,68 @@ def register(request):
             
         
     return render(request, 'register.html')
+
+def send_email(email):
+    pass
+
+def reset(request):
+    if request.method == 'POST':
+        print('postMethod')
+        username = request.POST.get('username')
+        if not User.objects.filter(username = username).exists():
+            return render(request,  'login.html', {
+            'error':"OTP doesn't match try again"
+        })
+        print(username)
+        user = User.objects.get(username = username)
+        print(user)
+        email = user.email
+        print(email)
+        # Random number generation.
+        Random = 123
+        # send_email(email, Random)
+        return render(request, 'reset_pass.html', {
+            'key': Random,
+            'username': username,
+            'email':email
+        })
+    print('GET methods')
+    return render(request, 'reset_pass.html')
+
+@require_http_methods(['POST'])
+def confirm(request, key, username):
+    # return render(request, 'home.html')
+    Key = request.POST.get('otp')
+    key = f"{key}"
+    print('inside confirm function')
+    print(type(key), type(Key))
+    if key == Key:
+        print('inside reset_pass for changing password')
+        return render(request, 'reset_pass.html', {
+            'password': True,
+            'username':f"{username}"
+        })
+    else:
+        return render(request, 'login.html', {
+            'error':"OTP doesn't match try again"
+        })
+@require_http_methods(['POST'])
+def setpass(request, username):
+    print('inside setpass fuction')
+    password = request.POST.get('password1')
+    confirm = request.POST.get('password2')
+    print(password)
+    print(confirm)
+    if password!=confirm:
+        return render(request, 'reset_pass.html', {
+            "error":"Password didn't match try again"
+        })
+    user = User.objects.get(username = username)
+    user.set_password(password)
+    user.save()
+    return render(request, 'login.html')
+    
+
 @login_required
 def logout(request):
     global username
